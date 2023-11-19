da92f51dbcbf60d22b4da258d25cabcf0e9c355e
Quinn F <145919080+Q-F1@users.noreply.github.com>
2023-11-19 14:52:49 -0500

    Car Class Base

--- src/main/java/com/example/groupproject/Meep.java
+++
@@ -1,8 +0,0 @@
-package com.example.groupproject;
-
-public class Meep extends Car {
-    public Meep(String type, String color, String shippingLocation, String paymentType, double totalCost) {
-        super(type, color, shippingLocation, paymentType, totalCost);
-        // Initialize Meep-specific instance variables
-    }
-}
