fdcd23db6c6034c8b677e6f26eeb44db632d3d33
EnriqueLeonR <enriqueleon002@gmail.com>
2023-11-20 16:47:35 -0500

    ShippingInfo Start

--- src/main/java/com/example/groupproject/ShippingInfo.java
+++ src/main/java/com/example/groupproject/ShippingInfo.java
@@ -2,4 +2,50 @@ package com.example.groupproject;
 
 public class ShippingInfo {
 
+
+    private String newBern = "Address: 3403 Dr. M.L.K. Jr Blvd, New Bern, NC 28562 \n" +
+            "Phone Number: (252)560-2311 \n" +
+            "Email: NewBernCapeFearCars@gmail.com";
+    private String Wilmington = "Address: 5725 Market St, Wilmington, NC 28405 \n" +
+            "Phone Number: (252)757-3849 \n" +
+            "Email: WilmingtionCapeFearCars@gmail.com";
+    private String Raleigh = "Address: 6005 Grove Church Rd suite b, Raleigh, NC 27612 \n" +
+            "Phone Number: (919)-845-2321 \n" +
+            "Email: RalieghCapeFearCars@gmail.com";
+    private String Boone = "Address:  2015 Blowing Rock Rd, Boone, NC 28607 \n" +
+            "Phone Number: (828) 414-9191 \n" +
+            "Email: BooneCapeFearCars@gmail.com";
+    private String GreensBoro = "Address: 3800 Burlington Rd, Greensboro, NC 27405 \n" +
+            "Phone Number: (336)-874-9376 \n" +
+            "Email: GreensBoroCapeFearCars@gmail.com";
+    private String Charlotte = "Address: 5555 Concord Pkwy S, Concord, NC 28027 \n" +
+            "Phone Number: (704)-564-1239 \n" +
+            "Email: CharlotteCapeFearCars@gmail.com";
+
+    public ShippingInfo (){};
+
+    public String getNewBern() {
+        return newBern;
+    }
+
+    public String getWilmington() {
+        return Wilmington;
+    }
+
+    public String getRaleigh() {
+        return Raleigh;
+    }
+
+    public String getBoone() {
+        return Boone;
+    }
+
+    public String getGreensBoro() {
+        return GreensBoro;
+    }
+
+    public String getCharlotte() {
+        return Charlotte;
+    }
+
 }
