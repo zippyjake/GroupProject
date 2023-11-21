53ae66fdc81dc791cb0cc4a458a6814e4b496b90
BCB6999 <151577962+BCB6999@users.noreply.github.com>
2023-11-21 16:58:21 -0500

    mangler desc update

--- src/main/java/com/example/groupproject/Mangler.java
+++ src/main/java/com/example/groupproject/Mangler.java
@@ -7,6 +7,6 @@ public class Mangler extends Meep{
     }
     //detailed description
     public String getDesc(){
-        return String.format("Make: %s, Model: %s, Total Cost: %f",this.getMake(),this.getModel(),this.getTotalCost());//PLACEHOLDER
+        return String.format("Meet the Mangler—a car that seamlessly combines luxury with sports, ready to conquer both city streets and off-road adventures. With its plush interiors and high-tech features, it offers a smooth and sophisticated ride in urban landscapes. It's also built to handle rugged terrains, providing an exhilarating off-road experience. Experience the best of both worlds with the Mangler, a versatile ride that effortlessly blends luxury, sports, and off-road capability for an extraordinary driving journey.",this.getMake(),this.getModel(),this.getTotalCost());//PLACEHOLDER
     }
 }
